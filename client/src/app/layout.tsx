4d88cda866709cc2f3863bc7e2e48fb500f379ce
Lucio Gastellu Arrieta <luciogastellu.dev@gmail.com>
2024-08-21 12:05:41 -0300

    feat: google analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,13 +1,26 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "600", "900"] });
 
 export const metadata: Metadata = {
   title: "Lucio Gastellu",
   description: "Lucio Gastellu professional portfolio",
-  keywords: ['Professional', 'Web', 'Development', 'UI', 'Data', 'Dev', 'Frontend', 'Backend', 'Software', 'System', 'Databases'],
+  keywords: [
+    "Professional",
+    "Web",
+    "Development",
+    "UI",
+    "Data",
+    "Dev",
+    "Frontend",
+    "Backend",
+    "Software",
+    "System",
+    "Databases",
+  ],
 };
 
 export default function RootLayout({
@@ -19,8 +32,23 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="shortcut icon" href="MeMemoji.webp" type="image/x-icon" />
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XKW12ZSZDJ"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XKW12ZSZDJ');
+          `}
+        </Script>
       </head>
-      <body className={`${poppins.className} bg-[#191919] scroll-smooth`}>{children}</body>
+      <body className={`${poppins.className} bg-[#191919] scroll-smooth`}>
+        {children}
+      </body>
     </html>
   );
 }
